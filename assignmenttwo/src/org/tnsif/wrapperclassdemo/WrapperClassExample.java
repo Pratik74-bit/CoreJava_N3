940f6f2a8631313a3c772bdb10fdbff010e70ac7
Pratik <Pratik@LAPTOP-SSGOMGS4>
2023-02-13 00:09:05 +0530

    first commit

---
+++ assignmenttwo/src/org/tnsif/wrapperclassdemo/WrapperClassExample.java
@@ -0,0 +1,13 @@
+package org.tnsif.wrapperclassdemo;
+
+public class WrapperClassExample {
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNSIF");
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[0]);
+		int res=a+b;
+		System.out.println(res);
+	}
+
+}
